c9524647e6c579c383a9a2e9b3be3eb92f204607
hakimbg1 <74165701+hakimbg1@users.noreply.github.com>
2024-02-27 12:25:28 +0100

    test

--- src/tests/math.test.js
+++ src/tests/math.test.js
@@ -1,6 +1,7 @@
 const { addNumbers } = require('../math');
 
 test('adds two numbers correctly', () => {
-  const result = addNumbers(10, 10);
-  expect(result).toBe(20);
+  const result = addNumbers(2, 3);
+  expect(result).toBe(5);
 });
+
