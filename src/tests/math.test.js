917cdaec3f8fe3abcbfe14d3181ac6bfd58b8bcb
hakimbg1 <74165701+hakimbg1@users.noreply.github.com>
2024-02-27 11:43:39 +0100

    Update math.test.js

--- src/tests/math.test.js
+++ src/tests/math.test.js
@@ -4,3 +4,8 @@ test('adds two numbers correctly', () => {
   const result = addNumbers(2, 3);
   expect(result).toBe(5);
 });
+
+test2('adds two numbers correctly', () => {
+    const result = addNumbers(2, 3);
+    expect(result).toBe(6);
+  });
